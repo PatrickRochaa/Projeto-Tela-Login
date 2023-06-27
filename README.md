ecde8c0d887a29a4d243a795177170507d662910
Patrick Rocha <patrick.rochapereira@hotmail.com>
2023-06-27 15:56:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-Tela-Login
+ Projeto de Tela Login – criada no modulo 04 no curso de HTML & CSS – do curso em vídeo ministrado pelo Professor Gustavo Guanabara.
---
+++ login.js
@@ -0,0 +1,3 @@
+console.log('Está pagina foi criada apenas para estudo não houve captura de nenhum dado seu e para que não houvesse download em seu dispositivo.'<br>
+
+'Obrigado.');
